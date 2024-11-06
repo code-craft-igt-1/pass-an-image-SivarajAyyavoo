22c9c3868dfa5d013ade6fbf89d1d24507292e67
SivarajAyyavoo <41547753+SivarajAyyavoo@users.noreply.github.com>
2024-11-06 07:57:56 +0530

    test

--- pass-an-image.cpp
+++ pass-an-image.cpp
@@ -5,6 +5,16 @@ int main() {
     Image image;
     image.rows = 512;
     image.columns = 512;
+
+    std::cout << "Generate an image" << std::endl;
+    // Fill the array with random pixel values (0 to 255)
+    for (int i = 0; i < 512; i++) {
+        for (int j = 0; j < 512; j++) {
+            int index = i * 512 + j;
+            image.pixels[index] = rand() % 256;  // Random value between 0 and 255
+        }
+    }
+
 	std::cout << "Brightening a 512 x 512 image\n";
     ImageBrightener brightener(image);
     int attenuatedCount = brightener.BrightenWholeImage();
